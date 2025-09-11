a66c62c919f85560662ad3c9a60d1e9ba89d198c
uosq <lboxwallytf2@gmail.com>
2025-09-11 14:38:11 -0300

    first commit

---
+++ changelog_table.lua
@@ -0,0 +1,38 @@
+return {
+    {
+        "2nd Sep 2025",
+        "Added missing servers into our region selector - Atlanta, Buesnos Aires, Helsinki, Korea",
+        "Added 'Rain Effect' option into Visuals tab",
+        "Fixed 'Auto Zoom', 'Wait For Charge' and 'Triggerbot' support for 'The Machina' and 'The Classic' sniper rifle",
+        "Fixed Bow aimbot sometimes didn't activate on aim key release with autoshoot enabled",
+        "Fixed Bow aim key wouldn't shoot if already charged and 'wait for charge' was off",
+        "Fixed SkinChanger sometimes applying skins to wrong weapon slot for Spy class",
+        "Fixed 'Kill Effect' option to be applied immediately without respawning",
+        "Fixed 'Prefer Medics' option not working in some cases",
+        "Fixed 'Ignore Cloaked' option to support third party community server plugins",
+        "Fixed Auto Backstab in Rage mode sometimes not using whole weapon range",
+        "Changed option name 'Remove unprintable' to 'Trim Chat Whitespace'",
+        "Minimal priority option is now only active if Player List feature is enabled",
+        "Hidden Name Stealer options, the options are now available via LUA API gui class",
+        "Lua: Fixed engine.Notification and gamecoordinator.EnumerateQueueMapsHealth",
+    },
+
+    {
+        "9th Jul 2025",
+        "Added StatTrack to the Skinchanger",
+        "Added a button to craft an inventory item from your current weapon in the Skinchanger, preserving all options (StatTrack, paint, etc.)",
+        "Added a search for unusual effects and colors in the Skinchanger",
+        "Fixed the Skinchanger so you can change the style of crafted inventory items",
+        "Changed the Skinchanger to automatically accept newly created items and display them on-screen",
+        "Updated the 'Weapon:' label in the Skinchanger-clicking it now switches to your next weapon",
+        "Added Unicode character support in all Skinchanger search fields",
+        "Added dropdown menus in the Lmaobox UI for selecting options with your mouse",
+        "Updated the follow bot so it copies the target's viewangles while walking",
+        "Changed 'Heal/Buff Weapons' default value to 'Both Teams', removed Wrench from this feature and added info panel warning if this option is affecting your current weapon",
+    },
+
+    {
+        "10th Jun 2025 (Loader)",
+        "Fixed a problem that caused a \"network error\" message to show after logging into Loader",
+    },
+}
